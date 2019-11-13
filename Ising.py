89df3a378069bb34f1c4fcf83271254afe2bd537
Giulio Bonelli <giulio.bonelli@studio.unibo.it>
2019-11-13 14:22:21 +0100

    I'm just inserting the skeleton of the class Ising which will be the skeleton of the first part of my project

---
+++ Ising.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 14:06:57 2019
+
+@author: Giulio
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 14:01:39 2019
+@author: Jonathan Frassineti
+"""
+import numpy as np
+from numpy.random import rand
+import random as rd
+    
+
+class Ising:
+	
+	def __init__()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
