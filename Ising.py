df85234e3123d67063f6ee9853ff9d85955819ff
Giulio Bonelli <giulio.bonelli@studio.unibo.it>
2019-11-13 23:44:07 +0100

    Verson of class Ising with all the main method working

--- Ising.py
+++ Ising.py
@@ -10,51 +10,87 @@ Created on Wed Nov 13 14:06:57 2019
 Created on Tue Oct 15 14:01:39 2019
 @author: Jonathan Frassineti
 """
+import random as rd
 import numpy as np
 from numpy.random import rand
-import random as rd
-import math
+from tkinter import *
 
 class Ising:
 	
-	def __init__(self,dimension,temperature=1):
-		self.lattice     = np.array([[0.5*rd.choice([-1,1])for j in range(dimension)]for i in range(dimension)])
-		self.dimension   = dimension
-		self.temperature = temperature
 	
-	def __str__(self):
+	def __init__(self,dimension,T=.1):
+		self.dimension = dimension
+		self.lattice  = np.array([[rd.choice([-0.5,0.5]) for i in range(dimension)]for j in range(dimension)])
+		self.T = T
+
+	def Make_move(self):
+		x,y = [rd.choice([i for i in range(self.dimension)]),rd.choice([i for i  in range(self.dimension)])]
+		neighbours = self.lattice[(x+1)%self.dimension,y]+self.lattice[x,(y+1)%self.dimension]+self.lattice[(x-1)%self.dimension,y]+self.lattice[x,(y-1)%self.dimension]
+		delta_energy_spot = 2*self.lattice[x,y]*neighbours
+		
+		if(delta_energy_spot<0):
+			self.lattice[x,y]*=-1
+			
+		elif(rand()<np.exp(-delta_energy_spot/self.T))	:
+			self.lattice[x,y]*=-1
+		
+		
+	def print_lattice(self):
+		Lattice = [['' for i in range (self.dimension)]for j in range (self.dimension)]
 		for i in range(self.dimension):
 			for j in range(self.dimension):
-				if(self.lattice[i,j])<0:
-					print('1',end='  ')
-				elif(self.lattice[i,j]>0):
-					print('0',end='  ')
-			print('\n')
-			
-	def Tot_Magn(self):
-		return sum(sum(self.lattice))
-
-	def Make_Metropolis_Move(self):
-		x,y                = np.array([rd.choice(list(range(self.dimension))),rd.choice(list(range(self.dimension)))])
-		neighbouring_sites = self.lattice[(x+1)%self.dimension,y]+self.lattice[x,(y+1)%self.dimension]+self.lattice[(x-1)%self.dimension,y]+self.lattice[x,(y-1)%self.dimension]
-		energy_change 	   = 2.*self.lattice[x,y]*neighbouring_sites
-		if(energy_change<0):
-			self.lattice[x,y]*=-1
-		elif(rand()<math.exp(-energy_change)/self.temperature):
-			self.lattice[x,y]*=-1
-		return self.lattice
-
-	def Montecarlo_Simulation(self,Nsteps):
+				if self.lattice[i,j]<=0:
+					Lattice[i][j] = '0' 
+				elif self.lattice[i,j]>0:
+					Lattice[i][j] = '1'
+		for i in range(self.dimension):
+			print(' '.join(Lattice[i]))
+		
+					
+	def print_lattice_GUI(self):
+		root = Tk()
+		Lattice = [['' for i in range(self.dimension)]for j in range(self.dimension)]			
+		for i in range(self.dimension):
+			for j in range(self.dimension):
+				if self.lattice[i,j]<=0:
+					Lattice[i][j]=Label(bg='white',width=2,height=2)
+					Lattice[i][j].grid(row=i,column=j)
+				if self.lattice[i,j]>=0:
+					Lattice[i][j]=Label(bg='black',width=2,height=2)
+					Lattice[i][j].grid(row=i,column=j)
+		mainloop()			
+				
+	def Abs_Tot_Magn(self):
+		return np.abs(2*sum(sum(self.lattice))/(self.dimension*self.dimension))
+	
+	
+				
+	def MontSim (self,Nsteps=1000000):
 		for i in range(Nsteps):
-			self.Make_Metropolis_Move()
-			if((i%10000==0)):
-				self.__str__()
-				print(self.Tot_Magn())
+			self.Make_move()
+			
+			procedure = Nsteps/(i+1)	
+			if procedure  in list(range(100)):
+				print(self.Abs_Tot_Magn())
+				print("{:.2f}".format(float(i/Nsteps)*100),'%\n')
+				
 
 
 
-ising = Ising(20)
-
-ising.Montecarlo_Simulation(100000)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				
